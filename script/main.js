b42f19fb29e68356e78b398cd502d40f83affa96
Andy Zhang <andzhng@gmail.com>
2014-10-26 14:17:40 -0400

    Added gitignore, formatted JS code

--- script/main.js
+++ script/main.js
@@ -14,90 +14,73 @@ define(['jquery'],function ($) {
     		var text = event.target.result;
     		var dataArray = JSON.parse(text);
 
-    		var map = {start:{'x':0,'y':1}};
+    		var map = {
+          start: {
+            'x': 0,
+            'y': 1
+          }
+        };
     		//map = new Map()
-    		
+
     		// Holds previous taxi coordinates
     		var prevCoord = null;
 
     		// Taxi state if has person or not
     		// Can add more properties if required.
-    		var state = {hasPerson: false};
+    		var state = { hasPerson: false };
 
 
-    		for(var i = 0; i < dataArray.length; i++){
-
+    		for (var i = 0; i < dataArray.length; i++) {
     			var data = dataArray[i];
 
-    			if(data.hasOwnProperty('action'))
-    			{
-    				if(data.action === 'pickup')
-    				{
-    					if(state.hasPerson)
-    					{
-    						console.log("Can't start new transaction, when you're in the middle of transaction (At index "+ i );
+    			if (data.hasOwnProperty('action')) {
+    				if (data.action === 'pickup') {
+    					if (state.hasPerson) {
+    						console.error("Can't start new transaction, when you're in the middle of transaction (At index "+ i );
     						return;
     					}
     					state.hasPerson = true;
-    				}
-    				else if(data.action === 'dropoff')
-    				{
 
-    					if(!state.hasPerson)
-    					{
-    						console.log("Can't end transaction, when you have no one in the cab (At index "+ i );
+    				} else if (data.action === 'dropoff') {
+    					if (!state.hasPerson) {
+    						console.error("Can't end transaction, when you have no one in the cab (At index "+ i );
     						return;
     					}
     					state.hasPerson = false;
     				}
-
     				continue;
 
-    			}
-    			else if(data.hasOwnProperty('x') && data.hasOwnProperty('y'))
-    			{
+    			} else if (data.hasOwnProperty('x') && data.hasOwnProperty('y')) {
     				coord = data;
-
-    			}
-    			else
-    			{
-    				console.log("Data at index" +i+ "has an invalid property");
+    			} else {
+    				console.error("Data at index" + i + "has an invalid property");
     				return;
     			}
 
     			// Make sure start state is valid
-    			if(prevCoord == null){
-
-    				if(!map.isStart(coord.x,coord.y)){
-    					console.log("Wrong start coordinates");
+    			if (prevCoord == null) {
+    				if (!map.isStart(coord.x,coord.y)) {
+    					console.error("Wrong start coordinates");
     					return;
     				}
     				prevCoord = coord;
+    			} else if (map.isStreet(coord.x,coord.y)) {
+            if((abs(coord.x - prevCoord.x) === 1
+                && abs(coord.y - prevCoord.y) === 0)
+                || (abs(coord.x - prevCoord.x) === 0
+                && abs(coord.y - prevCoord.y) === 1)) {
+              prevCoord = coord;
+            } else {
+              console.error("Taxi can't travel more than 1 unit horizontal or vertical " + i + "in data");
+              return;
+            }
+          } else {
+            console.error("Not valid coordinate at index " + i + "in data at coordinate (" +x+ "," + "y"+")");
+            return;
+          }
+        }
 
-    			}
-    			else if(map.isStreet(coord.x,coord.y))
-				{
-					if((abs(coord.x - prevCoord.x) === 1
-						&& abs(coord.y - prevCoord.y) === 0)
-						|| (abs(coord.x - prevCoord.x) === 0
-						&& abs(coord.y - prevCoord.y) === 1))
-					{
-						prevCoord = coord;
-					}
-					else
-					{
-						console.log("Taxi can't travel more than 1 unit horizontal or vertical " + i + "in data");
-						return;
-					}
-				}
-				else
-				{
-					console.log("Not valid coordinate at index " + i + "in data at coordinate (" +x+ "," + "y"+")");
-					return;
-				}
-			}
-
-    		alert("Good to go!");
+        alert("Good to go!");
   		};
     });
 });
